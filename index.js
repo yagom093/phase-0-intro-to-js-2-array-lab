ec8264b000de128022ed0f95b88bff85692b6bf2
yago <yagom10@hotmail.com>
2023-03-11 22:07:36 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)  
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name)
+}
+
+function removeLastCat(name) {
+    const newCats =cats.slice(0, cats.length-1)
+    return newCats
+}
+
+function removeFirstCat(name) {
+    const newCats = cats.slice(1)
+    return newCats
+}
+
+function appendCat(Broom){
+    const newCats = [...cats, Broom] 
+    return newCats
+}
+ function prependCat(Broom) {
+    const newCats = [Broom, ...cats]
+    return newCats
+ }
